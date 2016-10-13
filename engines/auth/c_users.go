86f727294e38c83210b404d924e20dcac88f2e8f
Jitang Zheng <jitang.zheng@gmail.com>
2016-10-12 17:06:32 -0700

    form.html

--- engines/auth/c_users.go
+++ engines/auth/c_users.go
@@ -7,5 +7,16 @@ import (
 )
 
 func (p *Engine) getUsersSignIn(c *gin.Context) {
-	c.HTML(http.StatusOK, "users/sign-in", gin.H{"locale": c.MustGet("locale")})
+	lang := c.MustGet("locale").(string)
+	c.HTML(http.StatusOK, "users/non-sign-in", gin.H{
+		"locale": lang,
+		"form": gin.H{
+			"title": p.I18n.T(lang, "auth.users.sign-in"),
+			"fields": []gin.H{
+				gin.H{"type": "email", "id": "email"},
+				gin.H{"type": "password", "id": "password"},
+				gin.H{"type": "password", "id": "passwordConfirm"},
+			},
+		},
+	})
 }
